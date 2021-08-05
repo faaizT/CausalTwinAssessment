f3cbf401389c36f72ac3a4385af751e95b42ac9d
faaizT <faaiztaufiq@gmail.com>
2021-08-05 13:04:07 +0100

    Half way through changing to bootstrapping in manski regression

--- ManskiBounds.py
+++ ManskiBounds.py
@@ -11,6 +11,7 @@ from sklearn.cluster import KMeans
 from scipy.stats import rankdata
 from manski_bounds.utils.Networks import *
 from manski_bounds.utils.HelperFunctions import *
+from sklearn.utils import resample
 from tqdm import tqdm
 import wandb
 
@@ -20,13 +21,25 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
-def main(args):
+def main_deprecated(args):
     MIMICtable_filtered_t0, MIMICtable_filtered_t1, MIMIC_data_combined, MIMICtable, pulse_data_combined, MIMICraw, pulseraw = preprocess_data(args)
     actionbloc = create_action_bins(MIMICtable_filtered_t0, args.nra)
     train_policies(MIMIC_data_combined, MIMICraw, actionbloc, args.models_dir, args.nr_reps)
     train_yobs(MIMIC_data_combined, MIMICtable_filtered_t0, MIMICraw, MIMICtable, actionbloc, args.models_dir, args.nr_reps, args.col_name)
     train_yminmax(MIMIC_data_combined, MIMICtable_filtered_t0, MIMICraw, MIMICtable, actionbloc, args.models_dir, args.nr_reps, args.col_name)
     train_ysim(MIMIC_data_combined, MIMICtable, pulse_data_combined, pulseraw, actionbloc, args.models_dir, args.nr_reps, args.col_name)
+
+
+def main(args):
+    MIMICtable_filtered_t0, MIMICtable_filtered_t1, MIMICtable, MIMICraw, pulseraw, pulse_data_t0, pulse_data_t1 = preprocess_data(args)
+    test_size = int(len(MIMICtable_filtered_t0)/5)
+    train_size = len(MIMICtable_filtered_t0) - test_size
+    MIMICtable_filtered_t0_tr = resample(MIMICtable_filtered_t0, n_samples=train_size)
+    MIMICtable_filtered_t1_tr = MIMICtable_filtered_t0_tr[['icustay_id']].merge(MIMICtable_filtered_t1, on='icustay_id')
+    actionbloc = create_action_bins(MIMICtable_filtered_t0, args.nra)
+    train_policies(MIMICtable_filtered_t0_tr, MIMICraw, actionbloc, args.models_dir, model=0)
+    train_yobs(MIMICtable_filtered_t0, MIMICtable_filtered_t1, MIMICtable_filtered_t0_tr, MIMICtable_filtered_t1_tr, MIMICraw, MIMICtable, actionbloc, args.models_dir, args.col_name, model=0)
+
 
 
 if __name__=="__main__":
@@ -40,7 +53,7 @@ if __name__=="__main__":
     parser.add_argument("--nra", help="Number of action bins", default=5, type=int)
     args = parser.parse_args()
 
-    wandb.init(project="Manski-Regression", name=f"{args.sim_name}-{args.col_name}-2")
+    wandb.init(project="Manski-Regression", name=f"{args.sim_name}-{args.col_name}-dryrun")
 
     main(args)
 
